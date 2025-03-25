e99f93837ae47ebf3309f8f4332cd50f7e3b36f1
Martin <20029@my.school.sanctamaria.school>
2025-03-26 12:59:44 +1300

    Welcoming part

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 # KFC Chat Bot
 # Programmer - Martin Rozik
+# Chat bot that takes customers order for KFC Botany Downs
+
+#Import library random
+import random
+#import random integer
+from random import randint
+
+#list of names used by BOT
+bot_names = ["Mark", "Pheobe", "Sally", "Micheal", "Denise",
+             "Ellen", "Eric", "Moana", "Lewis", "Lara"]
+
+def welcome ():
+    num = randint (0,9)
+    name = (bot_names[num])
+    print("*** Welcome to Dream Pizza ***")
+    print("*** My name is", name, "***")
+    print("*** I will be here to help you order your delicious Dream Pizza ***")
+
+
+
+
+
+
+
+
+
+
+def main():
+    welcome()
+
+
+main()
